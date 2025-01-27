f60c7df9e52f50ede5681c67c039601ac34fd77f
nehagmz <nehagomez18@gmail.com>
2025-01-27 19:28:27 +0530

    Backend initialized

---
+++ backend/src/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    import uvicorn # type: ignore
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload = True)
